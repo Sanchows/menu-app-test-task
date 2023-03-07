e313b85127cc1de56806a937dafeb706c1c0d6fe
Alexander Sachenko <sanyasach@mail.ru>
2023-03-07 13:21:47 +0300

    prepared to implement a collapsing/expanding items; some docker changes; cleaned code

--- entrypoint-web.sh
+++ entrypoint-web.sh
@@ -2,10 +2,6 @@
 
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py createsuperuser \
---noinput \
---username $DJANGO_SUPERUSER_USERNAME \
---email $DJANGO_SUPERUSER_EMAIL
 uvicorn config.asgi:application --host 0.0.0.0 --port 8000
 
 exec "$@"
--- src/project/menu/templatetags/menu_tags.py
+++ src/project/menu/templatetags/menu_tags.py
@@ -3,26 +3,27 @@ from django import template
 from menu import utils
 from menu.models import Item
 
+from pprint import pprint
+
+
 register = template.Library()
 
 
 @register.inclusion_tag("menu/menu.html")
 def draw_menu(name: str):
     menu = (
-        Item.objects.select_related("parent", "children")
+        Item.objects.select_related("parent")
         .values(
             "id",
             "name",
             "parent",
             "parent__name",
-            "children__name",
-            "children",
             "level"
         )
         .filter(menu__name=name)
         .order_by("level")
     )
 
-    menu = utils.transform(menu)
+    menu = utils.transform(tuple(menu))
 
     return {"menu": menu}
